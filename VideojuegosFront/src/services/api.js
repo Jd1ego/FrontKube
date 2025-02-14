71866be6950685761a7aa2decf1ebf62e670dacd
Jd1ego <140413545+Jd1ego@users.noreply.github.com>
2025-02-14 10:26:16 -0500

    cors

--- VideojuegosFront/src/services/api.js
+++ VideojuegosFront/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080"; 
+const API_URL = "http://backend-juegos-service:8080"; 
 
 export const getJuegos = async () => {
   const response = await axios.get(`${API_URL}/Juegos`);
