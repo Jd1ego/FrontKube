1f4142015bc1a33c8e56ff8935e6895bedacc471
Ayala217 <140421850+Ayala217@users.noreply.github.com>
2025-02-15 11:32:22 -0500

    Corrige conexiòn axios

--- VideojuegosFront/src/services/api.js
+++ VideojuegosFront/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://backend-juegos-service:8080"; 
+const API_URL = "/backend"; 
 
 export const getJuegos = async () => {
-  const response = await axios.get(`${API_URL}/Juegos`);
+  const response = await axios.get(API_URL + "/Juegos"); 
   return response.data;
 };
 
 export const addJuego = async (juego) => {
-  await axios.post(`${API_URL}/Juego`, juego);
+  await axios.post(API_URL + "/Juego", juego); 
 };
